d0dcf6e04901622804d0a480f5d598de2330beef
Damien Fourneaux <damien.fourneaux@epfedu.fr>
2022-11-18 17:10:12 +0100

    derniere version du 18/11

--- src/superpuissance4_fourneaux_pommarel/SuperPuissance4_FOURNEAUX_POMMAREL.java
+++ src/superpuissance4_fourneaux_pommarel/SuperPuissance4_FOURNEAUX_POMMAREL.java
@@ -14,7 +14,7 @@ public class SuperPuissance4_FOURNEAUX_POMMAREL {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
