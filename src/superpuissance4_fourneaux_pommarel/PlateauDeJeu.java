81b7280a6aac0a7c71a47a8a4608068bf7359b4d
Damien Fourneaux <damien.fourneaux@epfedu.fr>
2022-11-30 14:23:57 +0100

    début classe plateau jeu

---
+++ src/superpuissance4_fourneaux_pommarel/PlateauDeJeu.java
@@ -0,0 +1,15 @@
+package superpuissance4_fourneaux_pommarel;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Damien
+ */
+public class PlateauDeJeu {
+    Cellulegrille[][] grille = new Cellulegrille [6][7];
+    
+}
