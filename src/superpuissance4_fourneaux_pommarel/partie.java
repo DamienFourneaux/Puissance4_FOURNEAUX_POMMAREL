9262c8a90523f1f9d17a4a9e3e3c071bfbd256e6
quentin pommarel <quentin.pommarel@epfedu.fr>
2022-12-02 12:54:45 +0100

    debut partie

---
+++ src/superpuissance4_fourneaux_pommarel/partie.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package superpuissance4_fourneaux_pommarel;
+
+/**
+ *
+ * @author quent
+ */
+public class partie  {
+    private joueur [] listeJoueurs;
+    private joueur joueurCourant;
+    private plateaudejeu plateau;
+    public void partie (joueur j1,joueur j2){
+        listeJoueurs[0]=j1;
+        listeJoueurs[1]=j2;
+    }
+   public void attribuerCouleurAuxJoueurs(){
+       
+   }
+}
