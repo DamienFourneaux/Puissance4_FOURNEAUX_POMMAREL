77e06801cd0c68205db41180c366dfbdcc672185
quentin pommarel <quentin.pommarel@epfedu.fr>
2022-12-02 15:16:10 +0100

    fin de la methode creeretaffectercouleur

--- src/superpuissance4_fourneaux_pommarel/partie.java
+++ src/superpuissance4_fourneaux_pommarel/partie.java
@@ -4,6 +4,8 @@
  */
 package superpuissance4_fourneaux_pommarel;
 
+import java.util.Random;
+
 /**
  *
  * @author quent
@@ -17,6 +19,28 @@ public class partie  {
         listeJoueurs[1]=j2;
     }
    public void attribuerCouleurAuxJoueurs(){
-       
+       Random n =new Random();
+       int var=n.nextInt(2);
+       if (var==0){
+               listeJoueurs[0].affecterCouleur("jaune");
+               listeJoueurs[1].affecterCouleur("rouge");
+       }else{
+               listeJoueurs[0].affecterCouleur("rouge");
+               listeJoueurs[1].affecterCouleur("jaune");
+       }
    }
+public void créeretaffecterJeton(joueur j1){
+    if("rouge"==j1.getCouleur()){
+    for(int i=0;i<31;i++){
+        Jeton stockjeton=new Jeton("rouge");
+        j1.ajouterJeton(stockjeton);
+    }
+}
+      if("jaune"==j1.getCouleur()){
+    for(int i=0;i<31;i++){
+        Jeton stockjeton=new Jeton("jaune");
+        j1.ajouterJeton(stockjeton);
+    }
+}
+}
 }
