f1ae59b27ab8d484926c126c63ec2df494f6114f
Damien Fourneaux <damien.fourneaux@epfedu.fr>
2022-12-07 10:24:00 +0100

    Partie Main commentée (version finale sous réserve d'une modification en cas de pb)

--- src/superpuissance4_fourneaux_pommarel/SuperPuissance4_FOURNEAUX_POMMAREL.java
+++ src/superpuissance4_fourneaux_pommarel/SuperPuissance4_FOURNEAUX_POMMAREL.java
@@ -12,11 +12,12 @@ public class SuperPuissance4_FOURNEAUX_POMMAREL {
 
     /**
      * @param args the command line arguments
+     * La méthode main permet d'exécuter les classes et de lancer la partie
      */
      public static void main(String[] args) {
         
-        joueur j1 = new joueur("quentin");
-        joueur j2 = new joueur("damien");
+        joueur j1 = new joueur("Quentin");
+        joueur j2 = new joueur("Damien");
         
         partie Partie = new partie(j1, j2);
 
